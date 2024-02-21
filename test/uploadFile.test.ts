b940a8d95872cee76984e9625095142a4f77ebdc
Shanky Kalra <shankkalra@gmail.com>
2024-02-21 19:41:43 -0400

    popUp_ Handler

--- test/uploadFile.test.ts
+++ test/uploadFile.test.ts
@@ -4,7 +4,7 @@ describe('Upload File', () => {
     const filePath0 = './videos/file1.webm';
     const filePath1 = './videos/file2.webm';
 
-    test("Upload Files Using Set Input Files", async () => {
+    xtest("Upload Files Using Set Input Files", async () => {
         const browser = await chromium.launch({
             headless: false
         })
@@ -13,10 +13,26 @@ describe('Upload File', () => {
         await page.goto('https://www.sendgb.com/');
         await page.setInputFiles("//input[@name='qqfile']", [filePath0, filePath1]);
 
-        /*await page.close()
+        await page.close()
         await context.close()
         await browser.close()
-        */
 
     })
+
+    test("Upload Files Via On Function", async () => {
+        const browser = await chromium.launch({
+            headless: false
+        })
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('https://the-internet.herokuapp.com/upload');
+        page.on("filechooser", async (filechooser) => {
+            //await afilechooser.isMultiple();
+            await filechooser.setFiles([filePath0, filePath1])
+        })
+        await page.click(".example + div#drag-drop-upload", { force: true })
+
+
+    })
+
 }) 
